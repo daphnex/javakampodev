a450e1efe44846f2acecd734dfc8ebb11a624ed4
Defne Alver <defne.lvrhotmail.com@192.168.1.35>
2022-09-23 10:39:07 +0300

    Ödev devamı 3.

--- intro/src/intro/Main.java
+++ intro/src/intro/Main.java
@@ -5,21 +5,34 @@ class Main {
 		System.out.println("Hello world!");
 		
 		
-		int not = 55;
-		int kredi = 10000;
-		double notOrtalaması = 50.5;
-		String isim = " Defne ";
-		boolean sistemeGirisYapmisMi = true;
-
+		int sayi = 25;
+		if (sayi>20){
+			System.out.println("Sayı 20'den büyüktür.");
+			} 
+		else if (sayi==20 ){
+			System.out.println("Sayı 20'ye eşittir.");
+		}else {
+			System.out.println("Sayı 20'den küçüktür.");	
+		}
+		 
+		int sayi1 = 24;
+		int sayi2 = 25;
+		int sayi3 = 2;
+		int enBuyuk = sayi1;
 		
-		int ogrenciSayisi = 10;
-		System.out.println("Öğrenci sayım = 10");
-		System.out.println("Öğrenci sayım = 10");
-		System.out.println("Öğrenci sayım = 10");
-		System.out.println("Öğrenci sayım = 10");
+		if(enBuyuk<sayi2) {
+			enBuyuk = sayi2;	
+		}
+		
+		if(enBuyuk<sayi3) {
+			
+			enBuyuk = sayi3;
+		}
+ 
+		System.out.println("En büyük = + enBuyuk");
 		
 		
+		 
+			
 	}
-		
 }
-
---
+++ oop1/src/oop1/Main.java
@@ -0,0 +1,39 @@
+package oop1;
+
+public class Main {
+	//op object orriante program - nesne güdümlü
+	
+	public static void main (String[] args) {
+		String mesaj = "Vade Oranı";
+		
+		Product product1 =new Product ();
+		product1.setName("Delonghi Kahve Makinesi");
+		product1setDiscount(7)
+		
+		
+		Product product2 =new Product ();
+		
+		
+		Product product3 =new Product ();
+		
+		
+		Product [] products = {product1, product2, product3};
+		
+		
+		System.out.println ("<ul>");
+		for (Product product : products) {
+		System.out.println("<li>" + product.name + "</li>");	
+		
+			
+		}
+		
+		System.out.println ("</ul>");
+		
+		
+		
+		
+		
+	}
+	
+
+}
